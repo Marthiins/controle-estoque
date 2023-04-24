e44d810e10d3c3bb62c53438c7f2f582bcfab48d
Marthiins <sergio.marthiins@gmail.com>
2023-04-24 17:45:18 -0300

    Projeto criado

---
+++ src/main/java/br/com/unieuro/stock/ControlInStockApplication.java
@@ -0,0 +1,13 @@
+package br.com.unieuro.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlInStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlInStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unieuro/stock/ControlInStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unieuro.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlInStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
