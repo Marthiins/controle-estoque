b59d9ee7a0c2ba94ec40734fabfd45598d8c9fa1
Marthiins <sergio.marthiins@gmail.com>
2023-04-24 23:35:40 -0300

    findById categorias

--- src/main/java/br/com/unieuro/stock/controller/CategoriaController.java
+++ src/main/java/br/com/unieuro/stock/controller/CategoriaController.java
@@ -3,6 +3,7 @@ package br.com.unieuro.stock.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,5 +36,11 @@ public class CategoriaController {
 		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));//conversão passando essa categoria para categoriaDTO
 		
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> findById (@PathVariable Long id) { // Encontra uma categoria no banco por ID.
+		Categoria obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/br/com/unieuro/stock/service/CategoriaService.java
+++ src/main/java/br/com/unieuro/stock/service/CategoriaService.java
@@ -1,5 +1,7 @@
 package br.com.unieuro.stock.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,20 +10,27 @@ import org.springframework.stereotype.Service;
 
 import br.com.unieuro.stock.domain.Categoria;
 import br.com.unieuro.stock.repositories.CategoriaRepository;
+import br.com.unieuro.stock.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
-	private final CategoriaRepository repository; //Importando o repository
-	
+
+	private final CategoriaRepository repository; // Importando o repository
+
 	@Autowired
-	public CategoriaService(CategoriaRepository repository) { //Construtor do repository
+	public CategoriaService(CategoriaRepository repository) { // Construtor do repository responsavel pelo CRUD
 		this.repository = repository;
 	}
 
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String directions) {// pagição categoria findPage busca paginada																																																																										
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(directions), orderBy); // retorna um page request com os parametros passado																																																	
+		return repository.findAll(pageRequest);
+	}
 
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy , String directions){//pagição categoria findPage busca paginada
-	    PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(directions), orderBy); //retorna um page request com os parametros passado
-	    return repository.findAll(pageRequest);
+	public Categoria findById(Long id) {// Metodo buscar por ID
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado ID: " + " , Tipo: " + Categoria.class.getName())); // orElseThrow Fazendo um tratamento de excessão do erro do id
+																							
 	}
 }
